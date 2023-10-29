8581905a764919f90c57e85c4007c7a937cae827
forlabi <lukman_afolabi@outlook.com>
2023-10-29 11:39:04 +0100

    A feature that allows a user to select the character that would use to mask the selected words

--- REDACT/script.js
+++ REDACT/script.js
@@ -5,19 +5,20 @@ button.addEventListener("click", function (event) {
     const userInput = document.getElementById('text1').value;
     const userScramble = document.getElementById('text2').value;
     const finalOutput = document.getElementById('text3');
+    const hashKey = document.getElementById('hash').value;
     
 
-    scrambleInput(userInput, userScramble, finalOutput);
+    scrambleInput(userInput, userScramble, finalOutput, hashKey);
 });
 
 
 
-const scrambleInput = function (userEntry, wordToScramble, finalOutput) {
+const scrambleInput = function (userEntry, wordToScramble, finalOutput, keyToUse) {
     const wordsToMaskArray = wordToScramble.split(',').map(word => word.trim());
     let scrambledText = userEntry;
     for (let word of wordsToMaskArray){
         const regexPattern = new RegExp(`\\b${word}\\b`, 'gi');
-        scrambledText = scrambledText.replace(regexPattern, "*".repeat(word.length))
+        scrambledText = scrambledText.replace(regexPattern, keyToUse.repeat(word.length));
 
         
     }

b2faaca3ac78acfd49543a228d65f574d784e981
forlabi <lukman_afolabi@outlook.com>
2023-10-29 09:59:30 +0100

    This update allows the app to take multiple words to scramble

--- REDACT/script.js
+++ REDACT/script.js
@@ -11,15 +11,16 @@ button.addEventListener("click", function (event) {
 });
 
 
-function maskWord(match) {
-    return "*".repeat(match.length);
-  };
-const scrambleInput = function (userEntry, wordToScramble, finalOutput) {
-    
-    const regexPattern = new RegExp(`\\b${wordToScramble}\\b`, 'gi');
-    const scrambledText = userEntry.replace(regexPattern, maskWord)
 
-    finalOutput.value = scrambledText;
-    
+const scrambleInput = function (userEntry, wordToScramble, finalOutput) {
+    const wordsToMaskArray = wordToScramble.split(',').map(word => word.trim());
+    let scrambledText = userEntry;
+    for (let word of wordsToMaskArray){
+        const regexPattern = new RegExp(`\\b${word}\\b`, 'gi');
+        scrambledText = scrambledText.replace(regexPattern, "*".repeat(word.length))
+
+        
+    }
+    finalOutput.value = scrambledText; 
 }
 

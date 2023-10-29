9b44c33fc3aed6685ceae16b75c507de1a4c6029
forlabi <lukman_afolabi@outlook.com>
2023-10-29 18:18:46 +0100

    - This update includes a pop up that displays Total scanned words and character , Total scrambled words and characters and some polishings on the UI

--- REDACT/script.js
+++ REDACT/script.js
@@ -1,27 +1,72 @@
 const button = document.getElementById('btn'); // button element
+const closeButton = document.getElementById('close-btn'); // button element
 
-button.addEventListener("click", function (event) { //Event listener to action scrambling function
+
+button.addEventListener('click', function (event) { //Event listener to action scrambling function
     event.preventDefault();
     const userInput = document.getElementById('text1').value;
     const userScramble = document.getElementById('text2').value;
     const finalOutput = document.getElementById('text3');
     const hashKey = document.getElementById('hash').value;
     
+    // const wordCount = document.getElementById('word-count'); 
+    // const scramCount = document.getElementById('scramble-count'); 
+    // const popup = document.getElementById('popup');
+    
+       
 
     scrambleInput(userInput, userScramble, finalOutput, hashKey); //Scramblig function is called with arguments
 });
 
 
-//Scramblin function
+//Scrambling function
 const scrambleInput = function (userEntry, wordToScramble, finalOutput, keyToUse) { 
+    
+    const wordCount = document.getElementById('word-count'); 
+    const scramCount = document.getElementById('scramble-count'); 
+    const popup = document.getElementById('popup');
+    const chaCount = document.getElementById('character-count');
+    const scramChaCount = document.getElementById("scram-cha-count");
+    const count = document.querySelector('.count');
     const wordsToMaskArray = wordToScramble.split(',').map(word => word.trim());
     let scrambledText = userEntry;
-    for (let word of wordsToMaskArray){
-        const regexPattern = new RegExp(`\\b${word}\\b`, 'gi');
-        scrambledText = scrambledText.replace(regexPattern, keyToUse.repeat(word.length));
+    const words = scrambledText.match(/\w+/g); // Total words scanned
+  
+    const numWords = words.length;
+    let scramCharacter = 0; //Tracker for the number of characters to be scrambled
+    let scrambleCounter = 0; //Tracker for the number of words to be scrambled
 
+    // Loop to match and scramble selected words
+        wordsToMaskArray.forEach(word => {
+            const scrambledPattern = new RegExp(`\\b${word}\\b`, 'gi');
+            const matches = scrambledText.match(scrambledPattern);
+            if (matches) {
+                scrambleCounter += matches.length;
+                scramCharacter += matches.join('').length; // Gets the character count of the scrambled words
+                
+            }
+            scrambledText = scrambledText.replace(scrambledPattern, keyToUse.repeat(word.length));
+            
+        });
+        
+        finalOutput.value = scrambledText; // Final output text
+       
+        // Words stats popup car
+        popup.classList.remove('hidden');
+        wordCount.textContent  = numWords;
+        scramCount.textContent =  scrambleCounter; 
+        chaCount.textContent =  userEntry.length; 
+        scramChaCount.textContent = scramCharacter; 
+        
         
     }
-    finalOutput.value = scrambledText; 
-}
+    
 
+  
+    
+
+// Close button for the stats popup
+closeButton.addEventListener('click', (event) => {
+    popup.classList.add('hidden');
+    event.preventDefault();
+});
